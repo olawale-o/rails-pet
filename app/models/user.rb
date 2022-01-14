c212ca65ebd7789c63766ea70edaa2673a2b49be
olawale-o <omoogunolawale@gmail.com>
2022-01-14 17:47:45 +0100

    Fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-          :jwt_authenticatable, jwt_revocation_strategy: self
-  
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   has_many :dogs, dependent: :destroy
   has_many :user_accounts, dependent: :destroy
   validates :email, presence: true
