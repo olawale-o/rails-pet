017e4d179353988aac53ee4fba729c049d8c2c9b
olawale-o <omoogunolawale@gmail.com>
2022-01-14 15:45:27 +0100

    Create accounts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_144038) do
+ActiveRecord::Schema.define(version: 2022_01_14_144344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "breeds", force: :cascade do |t|
     t.string "name"
---
+++ spec/factories/accounts.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :account do
+    type
+  end
+end
