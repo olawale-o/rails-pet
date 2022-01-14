1e872584e4d0ceed81f38fe069480fba7e5abe2f
olawale-o <omoogunolawale@gmail.com>
2022-01-14 16:28:38 +0100

    Create dogs table

---
+++ db/migrate/20220114151840_create_dogs.rb
@@ -0,0 +1,13 @@
+class CreateDogs < ActiveRecord::Migration[7.0]
+  def change
+    create_table :dogs do |t|
+      t.string :name, null: false
+      t.string :color, null: false
+      t.string :weight, null: false
+      t.text :description, null: false
+      t.references :breed, null: false, foreign_key: true
+      t.references :owner, references: :users. foreign_key: { to_table: :users }, null: false
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/dogs.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :dog do
+    name
+  end
+end
