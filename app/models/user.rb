4f2921679400211cbbc61526dbf6fcfc1045ef0c
olawale-o <omoogunolawale@gmail.com>
2022-01-14 17:25:40 +0100

    Setup User model with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+          :jwt_authenticatable, jwt_revocation_strategy: self
+  
   has_many :dogs, dependent: :destroy
   has_many :user_accounts, dependent: :destroy
   validates :email, presence: true
