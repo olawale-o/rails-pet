b83bfd328584a02fe77e60a40dd1e9e95793e738
olawale-o <omoogunolawale@gmail.com>
2022-01-15 07:24:06 +0100

    Modify Dog model

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,6 @@
 class Dog < ApplicationRecord
   belongs_to :user, foreign_key: :owner_id, class_name: 'User', counter_cache: true
-  belongs_to :breed
+  belongs_to :breed, counter_cache: true
 
   validate_presence_of :name, :color, :weight, :description, :breed_id, :owner_id
   validates :name, uniqueness: { scope: :owner_id }
