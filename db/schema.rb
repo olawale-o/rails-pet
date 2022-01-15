71c45a652b3feb22382c0922e76745b729a168aa
olawale-o <omoogunolawale@gmail.com>
2022-01-15 07:23:43 +0100

    Migrate pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_14_161404) do
+ActiveRecord::Schema.define(version: 2022_01_15_062005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_01_14_161404) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "dogs_count", default: 0
   end
 
   create_table "dogs", force: :cascade do |t|
@@ -58,7 +59,9 @@ ActiveRecord::Schema.define(version: 2022_01_14_161404) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
