2e37098db41aa1efa4ee2ab92853df824036cd66
olawale-o <omoogunolawale@gmail.com>
2022-01-17 17:50:36 +0100

    Write test for user model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
+  sequence(:email) { |n| "test#{n}@email.com" }
+  sequence(:username) { |n| "test#{n}" }
   factory :user do
     email
+    username
+    password { 'password' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    let(:user) { build(:user) }
+    it 'username is required' do
+      user.username = nil
+      expect(user).to_not be_valid
+    end
+
+    it 'email is required' do
+      user.email = nil
+      expect(user).to_not be_valid
+    end
+  end
 end
