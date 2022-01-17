92b70441ba5e433a9953ead33af4fbb807ad012f
olawale-o <omoogunolawale@gmail.com>
2022-01-17 17:00:25 +0100

    User can login and register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  include ActionController::MimeResponds
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  respond_to :json
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
 end
