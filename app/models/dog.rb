5971aba65d2315dd22ddf187e4eb830f15a5e9c2
olawale-o <omoogunolawale@gmail.com>
2022-03-15 19:32:11 +0100

    Add more dog validation

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,6 @@ class Dog < ApplicationRecord
   belongs_to :user, foreign_key: :owner_id, class_name: 'User', counter_cache: true
   belongs_to :breed, counter_cache: true
 
-  validates_presence_of :name, :color, :weight, :description, :breed_id, :owner_id
+  validates_presence_of :name, :color, :weight, :description, :breed_id, :owner_id, :gender
   validates :name, uniqueness: { scope: :owner_id }
 end
