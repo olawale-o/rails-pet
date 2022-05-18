eb1417a9440b92d083c540711ddfe5998fce1eac
olawale-o <omoogunolawale@gmail.com>
2022-05-18 17:39:34 +0100

    Migrate like table

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_12_123311) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_05_18_163519) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "accounts", force: :cascade do |t|
     t.string "type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "active_storage_attachments", force: :cascade do |t|
@@ -26,7 +25,7 @@ ActiveRecord::Schema.define(version: 2022_05_12_123311) do
     t.string "record_type", null: false
     t.bigint "record_id", null: false
     t.bigint "blob_id", null: false
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
@@ -39,7 +38,7 @@ ActiveRecord::Schema.define(version: 2022_05_12_123311) do
     t.string "service_name", null: false
     t.bigint "byte_size", null: false
     t.string "checksum"
-    t.datetime "created_at", precision: 6, null: false
+    t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
@@ -51,8 +50,8 @@ ActiveRecord::Schema.define(version: 2022_05_12_123311) do
 
   create_table "breeds", force: :cascade do |t|
     t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "dogs_count", default: 0
   end
 
@@ -63,17 +62,26 @@ ActiveRecord::Schema.define(version: 2022_05_12_123311) do
     t.text "description", null: false
     t.bigint "breed_id", null: false
     t.bigint "owner_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "gender", null: false
     t.string "pic_url"
     t.index ["breed_id"], name: "index_dogs_on_breed_id"
     t.index ["owner_id"], name: "index_dogs_on_owner_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.bigint "dog_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dog_id"], name: "index_likes_on_dog_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
+  end
+
   create_table "user_accounts", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -81,14 +89,14 @@ ActiveRecord::Schema.define(version: 2022_05_12_123311) do
     t.string "last_name"
     t.string "username"
     t.string "phone_number"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "dogs_count", default: 0
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
@@ -99,4 +107,6 @@ ActiveRecord::Schema.define(version: 2022_05_12_123311) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "dogs", "breeds"
   add_foreign_key "dogs", "users", column: "owner_id"
+  add_foreign_key "likes", "dogs"
+  add_foreign_key "likes", "users"
 end
