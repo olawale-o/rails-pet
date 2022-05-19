cc1bb9e35dc9f96711e014a554fd57d563431200
olawale-o <omoogunolawale@gmail.com>
2022-05-19 02:45:19 +0100

    Change route http verb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   namespace :v1, defaults: { format: :json } do
     get '/dogs', to: 'dogs#dogs'
     get '/dogs/:id/photos', to: 'dogs#photos'
-    post '/dogs/:id', to: 'dogs#set_pet_photo'
+    put '/dogs/:id/photos', to: 'dogs#set_pet_photo'
     resources :breeds, only: [:index] do
       resources :dogs, only: [:index]
     end
