b39684ea1c732264205a928a85d176023486eac5
olawale-o <omoogunolawale@gmail.com>
2022-06-06 13:19:01 +0100

    Create likes_controller

---
+++ app/controllers/v1/likes_controller.rb
@@ -0,0 +1,8 @@
+class LikesController < ApplicationController
+  def create
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id
+    @like.save
+    redirect_to :back
+  end
+end
